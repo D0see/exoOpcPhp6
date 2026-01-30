2ad0c3c20fa3a428bd33e58e424d742c853bd9e6
D0see <317Y17@gmail.com>
2026-01-30 12:17:02 +0100

    first pass on home page css

--- app/views/components/main-button.php
+++ app/views/components/main-button.php
@@ -1,3 +1,3 @@
-<a class="main-button" href="index.php?action=<?= $action ?>">
+<a class="main-button <?= $type?>" href="index.php?action=<?= $action ?>">
     <p class="main-button-content"><?= $buttonContent ?></p>
 </a>
--- app/views/templates/home.php
+++ app/views/templates/home.php
@@ -2,7 +2,7 @@
     <div class="block-element">
         <div class="introduction">
             <div class="introduction-left">
-                <h2 class="block-element-title">
+                <h2 class="block-element-title align-left">
                     Rejoignez nos lecteurs passionés
                 </h2>
                 <p class="block-element-text">
@@ -11,6 +11,7 @@
                 <?php 
                     $buttonContent = "Découvrir";
                     $action = "viewLibrary";
+                    $type = "full-button";
                     require __DIR__ . '/../components/main-button.php'; 
                 ?>
             </div>
@@ -34,8 +35,56 @@
             <?php 
                 $buttonContent = "Voir tous les livres";
                 $action = "viewLibrary";
+                $type = "full-button";
                 require __DIR__ . '/../components/main-button.php'; 
             ?>
         </div>
+    </div>
+    <div class="block-element">
+        <div class="how-it-works">
+            <h2 class="block-element-title">
+                Comment ça marche ?
+            </h2>
+            <p class="block-element-text">
+                Échanger des livres avec TomTroc c’est simple et amusant ! Suivez ces étapes pour commencer :
+            </p>
+            <div class="display-simple-blocks">
+                <div class="simple-block">
+                    Inscrivez-vous gratuitement sur notre plateforme.
+                </div>
+                <div class="simple-block">
+                    Ajoutez les livres que vous souhaitez échanger à votre profil.
+                </div>
+                <div class="simple-block">
+                    Parcourez les livres disponibles chez d'autres membres.
+                </div>
+                <div class="simple-block">
+                    Proposez un échange et discutez avec d'autres passionnés de lecture.
+                </div>
+            </div>
+            <?php 
+                $buttonContent = "Voir tous les livres";
+                $action = "viewLibrary";
+                $type = "hollow-button";
+                require __DIR__ . '/../components/main-button.php'; 
+            ?>
+        </div>
+    </div>
+    <div class="block-element">
+        <div class="outro">
+            <img class="outro-image" src="assets/outro.png" alt="bibliotheque pleine de livres">
+            <div class="inner-outro">
+                <h2 class="block-element-title align-left">
+                    Nos valeurs
+                </h2>
+                <p class="block-element-text">
+                    Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes. Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé. Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.
+                </p>
+                <p class="block-element-subtitle">
+                    L’équipe Tom Troc
+                </p>
+                <img class="heart" src="assets/heart.png" alt="motif de coeur">
+            </div>
+        </div>  
     </div>
 </div>
