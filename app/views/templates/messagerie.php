f836bdb657d2bccae47cce34aaab90329469bb99
D0see <317Y17@gmail.com>
2026-02-04 22:24:59 +0100

    first pass on messagerie

--- app/views/templates/messagerie.php
+++ app/views/templates/messagerie.php
@@ -1,7 +1,7 @@
-<div class="messagerie">
-    <div class="messagerie__sidebar">
-        <h2 class="messagerie__title">Messagerie</h2>
-        <div class="messagerie__contacts">
+<div class="block-elem">
+    <div class="messagerie">
+        <div class="messagerie-left">
+            <h2 class="block-element-title messagerie-title">Messagerie</h2>
             <?php foreach ($lastMessages as $lastMessage): ?>
                 <?php 
                     [
@@ -10,47 +10,74 @@
                     ] = $lastMessage;
                 ?>
                 <a href="index.php?action=viewMessagerie&idContact=<?= $correspondant->getId() ?>" 
-                   class="contact-item">
-                    <div class="contact-item__preview">
-                        <?= htmlspecialchars($message->getContent()) ?>
-                    </div>
-                    <div class="contact-item__name">
-                        <?= htmlspecialchars($correspondant->getPseudo()) ?>
+                class="messagerie-contact">
+                    <img
+                        class="messagerie-contact-image"
+                        src="<?= htmlspecialchars($correspondant->getImage()) ?>"
+                        alt="<?= htmlspecialchars($correspondant->getPseudo()) ?>"
+                    >
+                    <div class="messagerie-contact-detail">
+                        <div class="messagerie-contact-top-row">
+                            <div>
+                                <?= htmlspecialchars($correspondant->getPseudo()) ?>
+                            </div>
+                            <div>
+                                <?= (new \DateTime($message->getCreatedAt()))->format('H:i') ?>
+                            </div>
+                        </div>
+                        <div class="messagerie-contact-message">
+                            <?= htmlspecialchars($message->getContent()) ?>
+                        </div>
                     </div>
                 </a>
             <?php endforeach; ?>
         </div>
-    </div>
-
-    <div class="messagerie__main">
-        <h2 class="messagerie__title">Conversation</h2>
-        
         <?php if (isset($conversation)): ?>
-            <div class="conversation">
-                <div class="conversation__messages">
+            <div class="messagerie-right">
+                <div class="messages">
                     <?php foreach ($conversation as $msg): ?>
-                        <div class="message">
-                            <div class="message__content">
-                                <?= htmlspecialchars($msg['message']->getContent()) ?>
+                        <?php if ($msg['sender']->getId() === $_SESSION['idUser']): ?>
+                            <div class="messagerie-message-block user-message">
+                                <div class="messagerie-conversation-message-info user-message-info">
+                                    <div>
+                                        <?= (new \DateTime($message->getCreatedAt()))->format('H:i') ?>
+                                    </div>
+                                </div>
+                                <div class="messagerie-conversation-message user-message">
+                                    <?= htmlspecialchars($msg['message']->getContent()) ?>
+                                </div>
                             </div>
-                            <div class="message__sender">
-                                <?= htmlspecialchars($msg['sender']->getPseudo()) ?>
+                        <?php else: ?>
+                            <div class="messagerie-message-block correspondant-message">
+                                <div class="messagerie-conversation-message-info correspondant-message-info">
+                                    <img
+                                        class="message-contact-image"
+                                        src="<?= htmlspecialchars($correspondant->getImage()) ?>"
+                                        alt="<?= htmlspecialchars($correspondant->getPseudo()) ?>"
+                                    >
+                                <div>
+                                        <?= (new \DateTime($message->getCreatedAt()))->format('H:i') ?>
+                                    </div>
+                                </div>
+                                <div class="messagerie-conversation-message correspondant-message">
+                                    <?= htmlspecialchars($msg['message']->getContent()) ?>
+                                </div>
                             </div>
-                        </div>
+                        <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
 
                 <form action="index.php?action=sendMessage&idContact=<?= $correspondant->getId() ?>" 
                       method="post" 
-                      class="message-form">
+                      class="messagerie-form">
                     <input type="text" 
                            name="message" 
                            id="message" 
-                           class="message-form__input" 
+                           class="block-element-input messagerie-input" 
                            placeholder="Votre message..."
                            required>
-                    <button type="submit" class="message-form__submit">
-                        Envoyer le message
+                    <button type="submit" class="main-button full-button messagerie-button">
+                        Envoyer
                     </button>
                 </form>
             </div>
